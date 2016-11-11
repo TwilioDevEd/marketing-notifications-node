375d700c0bee49bc76fcbbf067c26ddea7e7b154
juancarlospaco <juancarlospaco@gmail.com>
2016-11-11 15:15:01 -0300

    Remove Fake keys.

--- config.js
+++ config.js
@@ -13,13 +13,13 @@ cfg.secret = process.env.APP_SECRET || 'keyboard cat';
 // A good practice is to store these string values as system environment
 // variables, and load them from there as we are doing below. Alternately,
 // you could hard code these values here as strings.
-cfg.accountSid = process.env.TWILIO_ACCOUNT_SID || 'AC3f899f99b9ec999999ded99c09c99e09'; // fake
-cfg.authToken = process.env.TWILIO_AUTH_TOKEN || '89b09c9bff999999a9c9999c9eda9e91'; // fake
+cfg.accountSid = process.env.TWILIO_ACCOUNT_SID;
+cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
 
 // A Twilio number you control - choose one from:
 // https://www.twilio.com/user/account/phone-numbers/incoming
 // Specify in E.164 format, e.g. "+16519998877"
-cfg.twilioNumber = process.env.TWILIO_NUMBER || '+5411511111381'; // fake
+cfg.twilioNumber = process.env.TWILIO_NUMBER;
 
 // MongoDB connection string - MONGO_URL is for local dev,
 // MONGOLAB_URI is for the MongoLab add-on for Heroku deployment
@@ -27,3 +27,9 @@ cfg.mongoUrl = process.env.MONGOLAB_URI || process.env.MONGO_URL || 'mongodb://l
 
 // Export configuration object
 module.exports = cfg;
+
+
+if (!cfg.accountSid || !cfg.authToken || !cfg.twilioNumber) {
+  throw new Error('TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN' +
+                  ' and TWILIO_NUMBER must be set!.');
+};
