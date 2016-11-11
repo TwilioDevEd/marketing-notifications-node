1f3b4be8083e3dd4e9abc8aa9278d58bc76d16a4
juancarlospaco <juancarlospaco@gmail.com>
2016-11-11 00:38:34 -0300

    Bump package.json, Fix code to work with next-gen.

--- config.js
+++ config.js
@@ -9,21 +9,21 @@ cfg.secret = process.env.APP_SECRET || 'keyboard cat';
 
 // Your Twilio account SID and auth token, both found at:
 // https://www.twilio.com/user/account
-// 
+//
 // A good practice is to store these string values as system environment
 // variables, and load them from there as we are doing below. Alternately,
 // you could hard code these values here as strings.
-cfg.accountSid = process.env.TWILIO_ACCOUNT_SID;
-cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
+cfg.accountSid = process.env.TWILIO_ACCOUNT_SID || 'AC3f899f99b9ec999999ded99c09c99e09'; // fake
+cfg.authToken = process.env.TWILIO_AUTH_TOKEN || '89b09c9bff999999a9c9999c9eda9e91'; // fake
 
 // A Twilio number you control - choose one from:
 // https://www.twilio.com/user/account/phone-numbers/incoming
 // Specify in E.164 format, e.g. "+16519998877"
-cfg.twilioNumber = process.env.TWILIO_NUMBER;
+cfg.twilioNumber = process.env.TWILIO_NUMBER || '+5411511111381'; // fake
 
 // MongoDB connection string - MONGO_URL is for local dev,
 // MONGOLAB_URI is for the MongoLab add-on for Heroku deployment
-cfg.mongoUrl = process.env.MONGOLAB_URI || process.env.MONGO_URL
+cfg.mongoUrl = process.env.MONGOLAB_URI || process.env.MONGO_URL || 'mongodb://localhost:27017' // default
 
 // Export configuration object
 module.exports = cfg;
