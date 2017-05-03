a4f8d5902a9c7880bdb2c1cfc3bbf2cc8936c059
Hector Ortega <hector.oj@gmail.com>
2017-05-03 12:00:41 -0500

    Fix travis.yml

--- test/connectionHelper.js
+++ test/connectionHelper.js
@@ -2,25 +2,14 @@
 
 const mongoose = require('mongoose');
 const cfg = require('../config');
-const MongoInMemory = require('mongo-in-memory');
 
 
-if (!cfg.mongoUrlTest) {
-  throw new Error('MONGO_URL_TEST env variable not set.');
+if (!cfg.mongoUrl) {
+  throw new Error('MONGO_URL env variable not set.');
 }
 
-const port = 8000;
-const mongoServerInstance = new MongoInMemory(port);
-mongoServerInstance.start((error, config) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('Mongo-in-memory started successfully');
-  }
-});
-
 const connect = () => {
-  return mongoose.connect(cfg.mongoUrlTest);
+  return mongoose.connect(cfg.mongoUrl);
 };
 
 const disconnect = () => {
